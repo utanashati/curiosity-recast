cea4427c0aeaad2b26784e538e0f641ee089e758
utanashati <utanashati@mail.ru>
2019-11-22 09:12:16 +0100

    +

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
-python main.py --env-name "PongDeterministic-v4" --num-processes 16 --save-model-again-eps 10 --save-video-again-eps 1 --max-episodes 60 --random-seed --short-description "aws-nocuriosity"
-python main.py --env-name "PongDeterministic-v4" --num-processes 16 --save-model-again-eps 10 --save-video-again-eps 1 --max-episodes 60 --random-seed --short-description "aws-nocuriosity"
-python main.py --env-name "PongDeterministic-v4" --num-processes 16 --save-model-again-eps 10 --save-video-again-eps 1 --max-episodes 60 --random-seed --short-description "aws-nocuriosity"
+python main.py --env-name "PongDeterministic-v4" --num-processes 16 --save-model-again-eps 10 --save-video-again-eps 1 --max-episodes 60 --random-seed --short-description "aws-curiosity"
+python main.py --env-name "PongDeterministic-v4" --num-processes 16 --save-model-again-eps 10 --save-video-again-eps 1 --max-episodes 60 --random-seed --short-description "aws-curiosity"
+python main.py --env-name "PongDeterministic-v4" --num-processes 16 --save-model-again-eps 10 --save-video-again-eps 1 --max-episodes 60 --random-seed --short-description "aws-curiosity"
